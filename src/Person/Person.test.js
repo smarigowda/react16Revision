858a9f6b62caafb221f9d94667eb46b5ee069ed0
Santosh Marigowda <santosharakere@gmail.com>
2018-03-31 14:21:06 +0100

    [REAC-7] jest tests added to Person component

--- src/Person/Person.test.js
+++ src/Person/Person.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Person from './Person';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Person />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('shallow renders without crashing', () => {
+  shallow(<Person />);
+});
+
+it('renders a paragraph', () => {
+  const wrapper = shallow(<Person />);
+  const element = <p>I'm a Person</p>;
+  expect(wrapper.contains(element)).toEqual(true);
+})
