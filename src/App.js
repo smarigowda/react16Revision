deaefc87692e193b417bb2af35184b30675e1765
Santosh Marigowda <santosharakere@gmail.com>
2018-03-31 11:15:43 +0100

    [REAC-4] Person component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <h1>Hi I'm a React app</h1>
-      // </div>
-      React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'), React.createElement('p', null, 'yes it does!'))
+      <div className="App">
+        <h1>Hi I'm a React app</h1>
+        <p>This is really working !</p>
+        <Person/>
+        <Person/>
+        <Person/>
+      </div>
+      // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'), React.createElement('p', null, 'yes it does!'))
     );
   }
 }
