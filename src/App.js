14efc242162222a363e253f20de4483959d5cefd
Santosh Marigowda <santosharakere@gmail.com>
2018-04-01 09:44:36 +0100

    [REAC-11] click handler and unit test to check the click

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
   state = {
     persons: [
       { name: "Santosh", age: 45 },
       { name: "Roopa", age: 40 },
       { name: "Sukruthi", age: 14 }
-    ]
+    ],
+    other: 'Some other state'
+  }
+
+  switchNameHandler = () => {
+    console.log('Switch Name Handler Called...');
+    this.setState({
+      persons: [
+        { name: "Santosh Marigowda", age: 45 },
+        { name: "Roopa", age: 40 },
+        { name: "Sukruthi", age: 14 }
+      ]
+    })
   }
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React app</h1>
         <p>This is really working !</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Reading</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { shallow, configure } from 'enzyme';
+import { shallow, configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
@@ -23,3 +23,16 @@ it('App should have a button', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find('button')).toHaveLength(1);
 })
+
+it('Button should have a onClick handler', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('button').prop('onClick')).toBeDefined();
+  expect(wrapper.find('button').prop('onClick')); // this is a function.
+  expect(wrapper.find('button').props()).toHaveProperty('onClick');
+});
+
+it('Clicking on the button should call the event handler function', () => {
+  const wrapper = mount(<App />);
+  wrapper.find('button').simulate('click');
+  expect(wrapper.state('persons')[0].name).toEqual('Santosh Marigowda');
+});
