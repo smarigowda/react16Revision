ee785602f2b678b7c5c7723fcbf2f143c7cbbe45
Santosh Marigowda <santosharakere@gmail.com>
2018-04-23 20:19:13 +0100

    REAC-30 converted Persons into stateful component

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
-const persons = props => props.persons.map((d, index) => {
-  return  <ErrorBoundary key={d.id}>
-            <Person
-              name={d.name}
-              age={d.age}
-              click={ index => { props.deletePersonHandler(index) }}
-              changeHandler={ (event) => props.nameChangeHandler(event, d.id) } />
-          </ErrorBoundary>
-});
 
-export default persons;
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((d, index) => {
+      return  <ErrorBoundary key={d.id}>
+                <Person
+                  name={d.name}
+                  age={d.age}
+                  click={ index => { this.props.deletePersonHandler(index) }}
+                  changeHandler={ (event) => this.props.nameChangeHandler(event, d.id) } />
+              </ErrorBoundary>
+    });
+  }
+}
+
+export default Persons;
